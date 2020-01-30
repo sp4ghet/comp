bcdb765089fa92f67df36ed6b2ce55019808e743
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-01-30 22:50:07 +0000

    progress

--- abc153/f.cpp
+++ abc153/f.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using P = pair<int, int>;
+using P = pair<ll, ll>;
 
 #define rep(i, n) for (int i = 0; i < n; ++i)
 #pragma region Debug
@@ -29,8 +29,36 @@ int main()
     int n, d, a;
     cin >> n >> d >> a;
     vector<P> mons(n, P(0, 0));
+    vector<ll> dec(n + 1, 0);
     rep(i, n) cin >> mons[i].first >> mons[i].second;
 
-    
+    sort(mons.begin(), mons.end());
+
+    ll ans = 0;
+
+    int j = 0;
+    rep(i, n)
+    {
+        P now = mons[i];
+        while (j < n && mons[j].first <= now.first + 2 * d)
+        {
+            j++;
+        }
+
+        now.second -= a * dec[i];
+        int times = (now.second + a - 1) / a;
+        times = max(times, 0);
+        ans += times;
+        dec[i] += times;
+        // since the condition in the loop holds up to j-1
+        // monster j does not get hit, but we increment anyways with the dp.
+        // To account for this, subtract from monster j so all monsters up to j-1 "gets hit"
+        // but monster j doesn't receive this damage because we subtract
+        dec[j] -= times;
+        dec[i + 1] += dec[i];
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
