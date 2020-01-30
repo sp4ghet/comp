3acf896c56cd56e81a046eb342c475710cba3f01
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-01-30 22:12:55 +0000

    organize templates

---
+++ templates/dp.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#pragma region Debug
+template <typename T>
+void view(const std::vector<T> &v)
+{
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename T>
+void view(const std::vector<std::vector<T>> &vv)
+{
+    for (const auto &v : vv)
+    {
+        view(v);
+    }
+}
+#pragma endregion
+#pragma region chminmax
+template <typename T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+#pragma endregion
+
+int main()
+{
+
+    return 0;
+}
