32583f989a79b551cb0f329f5543e82b98917834
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-03-14 18:46:28 +0000

    progress

---
+++ abc156/e.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#pragma region Debug
+template <typename T>
+void view(const std::vector<T> &v)
+{
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename T>
+void view(const std::vector<std::vector<T>> &vv)
+{
+    for (const auto &v : vv)
+    {
+        view(v);
+    }
+}
+#pragma endregion
+
+#pragma region mod
+const int p = 1e9 + 7; //10^9 + 7
+struct mint
+{
+    ll x;
+    mint(ll x = 0) : x(x % p) {}
+    mint &operator+=(const mint a)
+    {
+        if ((x += a.x) >= p)
+            x -= p;
+        return *this;
+    }
+    mint &operator-=(const mint a)
+    {
+        if ((x += p - a.x) >= p)
+            x -= p;
+        return *this;
+    }
+    mint &operator*=(const mint a)
+    {
+        (x *= a.x) %= p;
+        return *this;
+    }
+
+    mint operator+(const mint a) const
+    {
+        mint res(*this);
+        return res += a;
+    }
+    mint operator-(const mint a) const
+    {
+        mint res(*this);
+        return res -= a;
+    }
+    mint operator*(const mint a) const
+    {
+        mint res(*this);
+        return res *= a;
+    }
+
+    mint pow(ll t) const
+    {
+        mint r = 1;
+        mint a = *this;
+        while (true)
+        {
+            if (t & 1)
+            {
+                r *= a;
+            }
+            t >>= 1;
+            if (t == 0)
+                break;
+            a *= a;
+        }
+
+        return r;
+    }
+
+    mint inv() const
+    {
+        return pow(p - 2);
+    }
+
+    mint &operator/=(const mint a)
+    {
+        return (*this) *= a.inv();
+    }
+};
+
+struct comb
+{
+    vector<mint> fact, invs;
+    comb(int n) : fact(n + 1), invs(n + 1)
+    {
+        assert(n < p);
+
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i)
+            fact[i] = fact[i - 1] * i;
+
+        invs[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i)
+            invs[i - 1] = invs[i] * i;
+    }
+    mint operator()(int n, int k)
+    {
+        if (k < 0 || k > n)
+            return 0;
+        return fact[n] * invs[n - k] * invs[k];
+    }
+};
+#pragma endregion
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    comb ncr(2 * n);
+
+    k = min(n, k);
+    mint ans = 0;
+    for (int i = 0; i <= k; ++i)
+    {
+        int x = n - i;
+        ans += ncr(n, i) * ncr(n - 1, x - 1);
+    }
+
+    cout << ans.x << endl;
+    return 0;
+}
